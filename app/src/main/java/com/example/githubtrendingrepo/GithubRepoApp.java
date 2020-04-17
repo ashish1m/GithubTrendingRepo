7d57be5f486db04011cd688c4cf93497ee5d8833
Ashish <mathua5@medtronic.com>
2020-04-17 23:34:17 +0530

    Created data feed in repository.

--- app/src/main/java/com/example/githubtrendingrepo/GithubRepoApp.java
+++ app/src/main/java/com/example/githubtrendingrepo/GithubRepoApp.java
@@ -2,9 +2,14 @@ package com.example.githubtrendingrepo;
 
 import android.app.Application;
 
+import com.example.githubtrendingrepo.executor.AppExecutor;
+import com.example.githubtrendingrepo.repository.Repository;
+import com.example.githubtrendingrepo.repository.db.AppDatabase;
+
 public class GithubRepoApp extends Application {
 
     private static GithubRepoApp sInstance;
+    private AppExecutor mAppExecutor;
 
     public static GithubRepoApp getInstance() {
         return sInstance;
@@ -15,5 +20,18 @@ public class GithubRepoApp extends Application {
         super.onCreate();
 
         sInstance = this;
+        mAppExecutor = new AppExecutor();
+    }
+
+    public AppDatabase getDatabase() {
+        return AppDatabase.getDatabase(this);
+    }
+
+    public Repository getRepository() {
+        return Repository.getInstance(getDatabase());
+    }
+
+    public AppExecutor getAppExecutor() {
+        return mAppExecutor;
     }
 }
--- app/src/main/java/com/example/githubtrendingrepo/repository/Repository.java
+++ app/src/main/java/com/example/githubtrendingrepo/repository/Repository.java
@@ -1,20 +1,64 @@
 package com.example.githubtrendingrepo.repository;
 
+import androidx.lifecycle.LiveData;
+
+import com.example.githubtrendingrepo.GithubRepoApp;
+import com.example.githubtrendingrepo.repository.db.AppDatabase;
+import com.example.githubtrendingrepo.repository.db.dao.GithubRepoDao;
+import com.example.githubtrendingrepo.repository.db.entity.GithubRepo;
+import com.example.githubtrendingrepo.repository.remote.api.ApiClient;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class Repository {
 
     private static Repository sInstance;
+    private final GithubRepoDao mGithubRepoDao;
+    private final LiveData<List<GithubRepo>> mGithubRepo;
 
-    private Repository() {
+    private Repository(AppDatabase db) {
+        mGithubRepoDao = db.githubRepoDao();
+        mGithubRepo = mGithubRepoDao.getAllRepo();
     }
 
-    public static Repository getInstance() {
+    public static Repository getInstance(AppDatabase db) {
         if (sInstance == null) {
             synchronized (Repository.class) {
                 if (sInstance == null) {
-                    sInstance = new Repository();
+                    sInstance = new Repository(db);
                 }
             }
         }
         return sInstance;
+    }
+
+    public void fetchGithubTrendingRepo() {
+        ApiClient.getInstance().getGithubRepoService().fetchTrendingRepository().enqueue(new Callback<List<GithubRepo>>() {
+            @Override
+            public void onResponse(Call<List<GithubRepo>> call, final Response<List<GithubRepo>> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    GithubRepoApp.getInstance().getAppExecutor().diskIO().execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            mGithubRepoDao.deleteAll();
+                            mGithubRepoDao.insertAll(response.body());
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<GithubRepo>> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
+    }
+
+    public LiveData<List<GithubRepo>> getGithubRepo() {
+        return mGithubRepo;
     }
 }
